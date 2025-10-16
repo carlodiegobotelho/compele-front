080562e1bffc9be3b7a53cd969923cb3c1bdca81
Carlo Botelho <carlodiegobotelho@gmail.com>
2025-10-16 02:57:07 -0300

    dns

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:61445",
+  baseURL: "https://compele.online/compele-api",
 });
 
 api.interceptors.request.use((config) => {
